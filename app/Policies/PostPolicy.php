4595837aac2a48b45220bf848a2f0d30d538478b
Mateus <mateuscirinosilva32@gmail.com>
2019-11-18 16:35:28 -0300

    criacao de um Polices para os Posts

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Policies;
+
+use App\Group;
+use App\Post;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create posts.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        $group = new Group;
+        $groups_allowed = $group->groups_allowed();
+
+        $user_groups = $user->groups;
+
+        foreach ($groups_allowed as $group_allowed)
+        {
+            foreach($user_groups as $user_group)
+            {
+                if($group_allowed->id === $user_group->id)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function update(User $user, Post $post)
+    {
+        return $user->owns($post);
+    }
+
+    /**
+     * Determine whether the user can delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function restore(User $user, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the post.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return mixed
+     */
+    public function forceDelete(User $user, Post $post)
+    {
+        //
+    }
+}
